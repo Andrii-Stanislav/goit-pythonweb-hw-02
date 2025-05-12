599d2726348763cc90879ff93cdfac0baa0b0ccc
Andrii Stanislav <66518828+Andrii-Stanislav@users.noreply.github.com>
2025-05-12 10:17:34 +0200

    hw 2: review fixes

--- conf/db.py
+++ conf/db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.environ["SQLALCHEMY_DATABASE_URL"]
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, max_overflow=5)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
